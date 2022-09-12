8085fe7a1854055e3073aca976cf29676424e121
malken21 <Malken_Marumasa@outlook.jp>
2022-09-12 12:29:17 +0900

    Add files via upload

---
+++ src/main/java/marumasa/send_morse_code/util/fromCodeMap.java
@@ -0,0 +1,12 @@
+package marumasa.send_morse_code.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class fromCodeMap {
+
+    public static Map<Character, String> codes = new HashMap<>();
+
+    static {
+    }
+}
